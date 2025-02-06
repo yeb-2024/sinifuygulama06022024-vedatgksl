68ce04179fc752e13ac5288a585ceab1d676d640
Vedat Göksal <vedatgoksal1@gmail.com>
2025-02-06 18:32:27 +0300

    Anasayfa Sınıfı Oluşturuldu

---
+++ SinifUyg/src/main/java/org/example/Anasayfa.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Anasayfa {
+
+    JFrame frame = new JFrame();
+
+    Anasayfa(){
+        frame.setSize(500, 500);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
