f5c608544ec078826af74e610c82bb04996cedbb
Vedat Göksal <vedatgoksal1@gmail.com>
2025-02-06 18:42:40 +0300

    Anasayfa Sınıfı Oluşturuldu

--- SinifUyg/src/main/java/org/example/Anasayfa.java
+++ SinifUyg/src/main/java/org/example/Anasayfa.java
@@ -5,10 +5,17 @@ import javax.swing.*;
 public class Anasayfa {
 
     JFrame frame = new JFrame();
+    JButton button = new JButton("Profili göster");
 
     Anasayfa(){
+        button.setBounds(200, 200, 100, 50);
+        button.setFocusable(false);
+
         frame.setSize(500, 500);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setLayout(null);
+
+        frame.add(button);
     }
 }
